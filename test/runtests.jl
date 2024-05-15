6049aa315da6b991bce03eaddb7b54f3a9df5a59
Jerome Guterl <guterlj@fusion.gat.com>
2024-05-14 23:54:22 -0700

    fix test

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,8 @@ using Test
 
 @testset "FusionSpecies.jl" begin
     ss = @species_set D C e⁻
-    FusionSpecies.set_main_species!(ss, D)
     FusionSpecies.import_species!(ss, @__MODULE__)
+    FusionSpecies.set_main_species!(ss, D)
+
     true
 end
